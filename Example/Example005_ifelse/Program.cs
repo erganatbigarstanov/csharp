a6000c8c6c1b3d25d34abaa6f6069bb3ede72d70
resp office <erganatking@gmail.com>
2023-02-03 20:00:33 +0600

    if

---
+++ Example/Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ведитее имя аользователя :");
+string username = Console.ReadLine();
+
+if ( username.ToLower() == "маша")
+{
+    Console.WriteLine("ура это Маша");
+}
+else
+{
+    Console.Write("привет");
+    Console.WriteLine(username);
+}
